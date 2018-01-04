d907637520a77c2bd6ec23ca83a84b704c996073
Suresh Balaji Moorthy <moorthyccs@gmail.com>
2018-01-03 23:50:21 -0800

    Uber Coding
    Uber Coding

--- uber-git/frontend/Home.js
+++ uber-git/frontend/Home.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import { Input } from 'antd';
+import { Row, Col } from 'antd';
+
 var Dimensions = require('react-dimensions');
 
 class Home extends Component {
   render() {
     return (
       <div>
-            Home
+        Home
+        <Input placeholder="Basic usage" />
+        <Row>
+          <Col span={2}></Col>
+          <Col span={10}>10</Col>
+          <Col span={10}>10</Col>
+          <Col span={2}></Col>
+        </Row>
       </div>
     );
   }
